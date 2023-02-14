3ded2c6037c5942a3868a0af13bfee5bd97169d0
erickeliasborges <50887364+erickeliasborges@users.noreply.github.com>
2023-02-13 22:20:53 -0300

    ajustes front

--- web-angular/src/app/pages/destinatarios/pipes/destinatario-grupos.pipe.ts
+++ web-angular/src/app/pages/destinatarios/pipes/destinatario-grupos.pipe.ts
@@ -2,16 +2,28 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { GrupoDestinatario } from '../../grupo-destinatarios/model/grupo-destinatario';
 
 @Pipe({
-    name: 'destinatariosGrupos'
+  name: 'destinatariosGrupos'
 })
 export class DestinatariosGruposPipe implements PipeTransform {
 
-    transform(value: GrupoDestinatario[]): any {
-        if (value && value.length) {
-            const firstItem = value[0];
-            return firstItem.id + ' - ' + firstItem.name + (value.length > 1 ? ', ... ' : '');
-        }
-        return '-'
-    }
+  transform(value: GrupoDestinatario[], showAll: boolean): any {
+    if (!value || !value.length)
+      return '-';
+
+    if (showAll)
+      return this.getAllGroups(value);
+
+    const firstItem = value[0];
+    return firstItem.id + ' - ' + firstItem.name + (value.length > 1 ? ', ... ' : '');
+  }
+
+  private getAllGroups(groups: GrupoDestinatario[]): string {
+    let all: string = "";
+    groups.forEach((value) => {
+      console.log(value);
+      all += (all !== "" ? ', ' : '') + value.id + ' - ' + value.name;
+    })
+    return all;
+  }
 
 }
